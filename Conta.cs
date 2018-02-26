e9b3fab3a1953ee2d8f3f0d95c6872f9c468cb58
MARCELOHBB <marcelo_aut@hotmail.com>
2018-02-26 17:43:36 -0300

    alterei classe para abstract

--- Conta.cs
+++ Conta.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CaixaEletronico
 {
-    class Conta
+    abstract class Conta
     {
         public int Numero { get; set; }
         public Cliente Titular { get; set; }
